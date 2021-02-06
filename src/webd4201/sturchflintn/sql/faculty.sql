99a31985f427568fb767b700948e85e4d18d6f3e
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2021-02-06 11:23:37 -0500

    Created the SQL Files and made sure they were working. Created the Database and Tables

--- src/webd4201/sturchflintn/sql/faculty.sql
+++ src/webd4201/sturchflintn/sql/faculty.sql
@@ -7,12 +7,18 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 DROP TABLE IF EXISTS faculty;
 CREATE TABLE faculty (
-id LONG FOREIGN KEY,
+id BIGINT PRIMARY KEY,
 school_code VARCHAR(5),
 school_description VARCHAR(255),
 office VARCHAR(5),
-phone_extension INT
+phone_extension INT,
+FOREIGN KEY(id) REFERENCES users(id)
 );
 
+INSERT INTO faculty (id, school_code, school_description, office, phone_extension) VALUES (100555666, 'HW', 'Hogwarts School of Wizarding', 'F-122', 1232);
+
+INSERT INTO faculty (id, school_code, school_description, office, phone_extension) VALUES (100858989, 'SR-AX', 'The Southern Reach', 'B-231', 2345);
+
+INSERT INTO faculty (id, school_code, school_description, office, phone_extension) VALUES (100600700, 'SGL', 'Saul Goodmans Lawyering School', 'L-333', 9999);
 
 SELECT * FROM faculty;
--- src/webd4201/sturchflintn/sql/students.sql
+++ src/webd4201/sturchflintn/sql/students.sql
@@ -7,11 +7,17 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 DROP TABLE IF EXISTS students;
 CREATE TABLE students (
-id LONG FOREIGN KEY,
+id BIGINT PRIMARY KEY,
 program_code VARCHAR(4),
 program_description VARCHAR(255),
-year INT
+year INT,
+FOREIGN KEY(id) REFERENCES users(id)
 );
 
+INSERT INTO students (id, program_code, program_description, year) VALUES (100222222, 'CPA', 'Computer Programmer Analyst', 3);
+
+INSERT INTO students (id, program_code, program_description, year) VALUES (100303769, 'CPA', 'Computer Programmer Analyst', 3);
+
+INSERT INTO students (id, program_code, program_description, year) VALUES (100111111, 'CSTY', 'Computer System Technology', 3);
 
 SELECT * FROM students;
